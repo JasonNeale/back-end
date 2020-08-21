d8d0feab478d275d2979c554b3a5ac579591b3e5
Jason Neale <jg13.1980@gmail.com>
2020-08-21 02:28:17 -0400

    Updated

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,11 @@ module.exports = {
   production: {
     client: 'sqlite3',
     connection: {
-      filename: './database/spotify-SS.db3'
+      host: "ec2-34-238-26-109.compute-1.amazonaws.com",
+      database: "d657adpq522u6o",
+      port: "5432",
+      user: "xkdvwlaaatecjp",
+      password: "bc11c76c0727de2bd3e0f8f0b58c97a6d50cab50c7eb3683ba2897e71db5069f",
     },
     useNullAsDefault: true,
     migrations: {
