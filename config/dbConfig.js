553acd6c4907493bd9fcd3827d9ea86f86eadccc
Jason Neale <jg13.1980@gmail.com>
2020-08-22 09:19:55 -0400

    Updated dbconfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
-require('cross-env')
 const knex = require('knex')
-const knexConfig = require('../knexfile.js')
+const environment = process.env.DB_ENV || 'development'
+const config = require('../knexfile.js')[environment]
 
-module.exports = knex(knexConfig.production)
+module.exports = knex(config)
