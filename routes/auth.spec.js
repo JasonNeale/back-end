712d0fecc2f54c078c527045db2780e74efa86a1
Jason Neale <jg13.1980@gmail.com>
2020-08-27 17:05:10 -0400

    Updated

--- routes/auth.spec.js
+++ routes/auth.spec.js
@@ -1,42 +1,43 @@
 const server = require('../server')
-const db = require('../database/dbConfig')
+const db = require('../config/dbConfig')
 const request = require('supertest')
 
 
-const user = {
-  username: 'Test User',
-  password: '1234567890'
-}
+const usera = {username: 'TestUserA', password: '1234567890'}
+const userb = {username: 'TestUserB', password: '1234567890'}
 
-describe('tests auth route', () => {
-  it('/api/auth/register - should return 201 (created)', () => {
-    const runTest = request(server)
-    .post('/api/auth/register')
-    .send(user)
-    .expect(201)
+describe('Auth Route Tests', () => {
+  it('/register - should return 201 (created)', () => {
+    const runTest = request(server).post('/api/auth/register').send(usera).expect(201)
   })
 
-  it('/api/auth/register - should return 400 (bad request)', async () => {
-    const runTest = await request(server)
-    .post('/api/auth/register')
-    .send('')
-    .expect(runTest.status).toBe(400)
+  it('/register - should return 400 (bad request)', async () => {
+    const runTest = await request(server).post('/api/auth/register').send('').expect(400)
   })
 
-  it('/api/auth/login - should return 401 (not found)', () => {
-    const runTest = request(server)
-    .post('/api/auth/login')
-    .send({
-      username: 'JoeBiden',
-      password: '987654321'
-    })
-    .expect(401)
+  it('/login - should return 401 (not found)', () => {
+    const runTest = request(server).post('/api/auth/login').send({username: 'JoeBiden', password: '987654321'}).expect(401)
   })
 
-  it('/api/auth/login - should return 500 (server error)', () => {
-    const runTest = request(server)
-    .post('/api/auth/login')
-    .send(user+user)
-    .expect(500)
+  it('/login - should return 500 (server error)', () => {
+    const runTest = request(server).post('/api/auth/login').send(usera+usera).expect(500)
+  })
+})
+
+describe('User Route Tests', () => {
+  it('/friends - should return 201 (created)', () => {
+    const runTest = request(server).post('/api/user/friends').send(user).expect(201)
+  })
+
+  it('Register - should return 400 (bad request)', async () => {
+    const runTest = await request(server).post('/api/auth/register').send('').expect(400)
+  })
+
+  it('Login - should return 401 (not found)', () => {
+    const runTest = request(server).post('/api/auth/login').send({username: 'JoeBiden', password: '987654321'}).expect(401)
+  })
+
+  it('Login - should return 500 (server error)', () => {
+    const runTest = request(server).post('/api/auth/login').send(user+user).expect(500)
   })
 })
